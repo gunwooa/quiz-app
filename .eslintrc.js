f8613f3aae83afbe4e16412f6620b5bb3161dd77
Leo <gunwoooa@naver.com>
2024-04-16 22:03:37 +0900

    chore : import 자동 정렬
    - https://manurana.medium.com/organizing-imports-in-react-and-react-native-faf982a3a3b5
    - https://velog.io/@eamon3481/eslint-%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4%EC%84%9C-import-%EC%88%9C%EC%84%9C%EB%A5%BC-%EC%9E%90%EB%8F%99%EC%9C%BC%EB%A1%9C-%EB%B0%94%EA%BF%94%EB%B3%B4%EC%9E%90

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community'],
+  plugins: ['import'],
+  rules: {
+    // this is for sorting WITHIN an import
+    'sort-imports': ['error', {ignoreCase: true, ignoreDeclarationSort: true}],
+    // this is for sorting imports
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['external', 'builtin'],
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+        ],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
